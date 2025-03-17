60568cdde6d20acb5e90fb415de6fbb90e83af34
Mitles1234 <miles.cutting@education.nsw.gov.au>
2025-03-17 15:10:29 +1100

    17/3/2025 - School
    Got the API working properly.

--- TESTFILE.py
+++ TESTFILE.py
@@ -1,13 +1,5 @@
 import requests
 
+joke = requests.get('https://joke.deno.dev/type/')
 
-url = 'https://api.jokes.one/jod?category=knock-knock'
-api_token = "YOUR API KEY HERE"
-headers = {'content-type': 'application/json',
-	   'X-JokesOne-Api-Secret': format(api_token)}
-
-response = requests.get(url, headers=headers)
-#print(response)
-#print(response.text)
-jokes=response.json()['contents']['jokes'][0]
-print(jokes)
+print(joke.json()['setup'], joke.json()['punchline'])
